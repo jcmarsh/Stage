07195a5df3c2f7a5ae550728ea42dc2b065658a2
James Marshall <jcmarsh@gwmail.gwu.edu>
2013-02-27 19:43:13 -0500

    Gridcar now has 16 sensors. Working on the potential controller.

--- experiments/potential_controller.py
+++ experiments/potential_controller.py
@@ -19,12 +19,25 @@ if c.connect() != 0:
 
 # proxy for position2d:0
 pos = playerc_position2d(c,0)
-if p.subscribe(PLAYERC_OPEN_MODE) != 0:
+if pos.subscribe(PLAYERC_OPEN_MODE) != 0:
+    raise playerc_error_str()
+
+# proxy for ranger:0
+ran = playerc_ranger(c,0)
+if ran.subscribe(PLAYERC_OPEN_MODE) != 0:
     raise playerc_error_str()
 
 # get the geometry
 if pos.get_geom() != 0:
     raise playerc_error_str()
-print("Robot size: (%.3f,#.3f)" % (pos.size[0], pos.size[1]))
+print "Robot size: (%.3f,%.3f)" % (pos.size[0], pos.size[1])
 
+print "Let's read some ranges! I'm excited. Are you?"
+for i in range (0, 15):
+    if i >= ran.scan_count:
+        break
+    scan_str += '%.3f ' % ran.ranges[i]
+print scan_str
+
+print("DONE!")
 
