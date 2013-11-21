298246697d7b0e386cb298fa9e9d617826e9472e
James Marshall <jcmarsh@gwmail.gwu.edu>
2013-11-21 15:24:39 -0500

    Added a test c controller for testing libplayerc.

--- experiments/run_player.py
+++ experiments/run_player.py
@@ -7,33 +7,36 @@
 # The second arg should be the file name of the picture file to use as the floor plan for player.
 import sys
 import shutil
-from subprocess import call
+import subprocess
 from os import path
 # needed to parse the name of the worldfile from the config file
 from stage_utils import *
 
 def print_n_quit():
-    print 'usage: python run_player.py [name_of_config.cfg] [name_of_map.png]'
+    print 'usage: python run_player.py [name_of_config.cfg] [name_of_world.world] [name_of_map.png]'
     exit()
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 4:
     print_n_quit()
 
 config_name = sys.argv[1]
-map_name = sys.argv[2]
+world_name = sys.argv[2]
+map_name = sys.argv[3]
 
-if path.splitext(config_name)[1] != ".cfg" or path.splitext(map_name)[1] != ".png":
+if path.splitext(config_name)[1] != ".cfg" or path.splitext(world_name)[1] != ".world" or path.splitext(map_name)[1] != ".png":
     print_n_quit()
 
+print "Config file: ", config_name
+print "World file: ", world_name
 print "Provided filename: ", map_name
 
-if WriteFloorOld(map_name) != 0:
+if WriteFloor(map_name) != 0:
     print "Failed to write floor.inc"
     exit()
 
 # Copy the world file to a temporary run file (so that the controller knows the name)
 new_world_name = "run_temp.world"
-old_world_name = search_text_property(config_name, "worldfile")
+old_world_name = "./worlds/" + world_name
 
 shutil.copyfile(old_world_name, new_world_name)
 
@@ -44,6 +47,7 @@ if WriteCFG(new_cfg_name, config_name, map_name) != 0:
     print "Failed to write the config file %s" % (new_cfg_name)
     exit()
 
-call(["player", new_cfg_name])
+
+player_id = subprocess.Popen(["player", new_cfg_name])
 
 print "Complete"
